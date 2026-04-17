180b6dcc17275d62ee1d12bfe2fe72b6e30c838b
klu <52183673+klu04@users.noreply.github.com>
2026-04-16 21:44:20 -0700

    Implementation of basic GET router for food_items

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,8 +1,44 @@
-from fastapi import FastAPI
+from app.food_data import food_items
+from fastapi import FastAPI, status
+from fastapi.exceptions import HTTPException
+from pydantic import BaseModel
 
 app = FastAPI()
 
+# Food Item Schema (for initial crud implementation)
+class FoodItemBase(BaseModel):
+    id: str
+    name: str
+    brand: str
+    barcode: str 
+    category: str 
+    image_url: str
 
+class FoodItemCreate(FoodItemBase):
+    pass
+    
+# Routers
 @app.get("/")
 async def read_msg():
-    return {"msg": "Hello World!"}
+    return {"msg": "Navigate to food-items"}
+
+@app.get("/food-items", response_model=list[FoodItemBase])
+async def get_all_food_items():
+    """Read all food items."""
+    return food_items
+
+@app.get("/food-items/{food_item_id}", response_model=FoodItemBase)
+def get_single_food_item(food_item_id: str):
+    """Get a specific food item by their ID
+
+    Args:
+        food_item_id (str): uuid of the food item.
+
+    Raises:
+        HTTPException: Returns a 404 response if food item does not exist.
+    """
+    for food_item in food_items:
+        if food_item.get("id") == food_item_id:
+            return food_item
+    raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Food Item not found")
+
