8a68ff49f372eceaafba91257a7ad6fc79f0e1ed
klu <52183673+klu04@users.noreply.github.com>
2026-04-20 16:33:22 -0700

    Restructure project files and directories - Move routers and schemas to module specific directory

--- backend/app/food_items/router.py
+++ backend/app/food_items/router.py
@@ -1,40 +1,75 @@
-from uuid import UUID
-from typing import Annotated
-from app.database.sqlconnector import get_db
-from app.database.models import FoodItems as FoodItemsModel
-from fastapi import APIRouter, Depends, HTTPException, status
-from ..schema import FoodItemCreate, FoodItemResponse, FoodItemUpdate
-from sqlalchemy import select
-from sqlalchemy.ext.asyncio import AsyncSession
+from uuid import uuid4
+from ..food_data import food_items_data
+from fastapi import APIRouter, HTTPException, status
+from ..food_items.schema import FoodItemCreate, FoodItemResponse, FoodItemUpdate
+
 
 router = APIRouter()
 
 
 @router.get("", response_model=list[FoodItemResponse])
-async def get_all_food_items(db: Annotated[AsyncSession, Depends(get_db)]):
-    """Read all food items (in ascending order)"""
-    result = await db.execute(select(FoodItemsModel).order_by(FoodItemsModel.name.asc()))
-    food_items = result.scalars().all()
-    return food_items
+async def get_all_food_items():
+    """Read all food items."""
+    return food_items_data
+
 
 @router.post("", response_model=FoodItemResponse, status_code=status.HTTP_201_CREATED)
-async def create_food_item(
-    food_item: FoodItemCreate, db: Annotated[AsyncSession, Depends(get_db)]
-):
-    """Create a Food Item"""
-    new_food_item = FoodItemsModel(**food_item.model_dump())
-    db.add(new_food_item)
-    await db.commit()
-    await db.refresh(new_food_item)
+async def create_food_item(food_item: FoodItemCreate):
+    # Creates a uuid
+    new_id = str(uuid4())
+    new_food_item = {
+        "id": str(new_id),  # Convert UUID to a string
+        "name": food_item.name,
+        "brand": food_item.brand,
+        "barcode": food_item.barcode,
+        "category": food_item.category,
+        "image_url": "https://image-of-food.com/products/",
+    }
+
+    food_items_data.append(new_food_item)
     return new_food_item
 
+
 @router.get("/{food_item_id}", response_model=FoodItemResponse)
-async def get_food_item(food_item_id: UUID, db: Annotated[AsyncSession, Depends(get_db)]):
-    result = await db.execute(
-        select(FoodItemsModel)
-        .where(FoodItemsModel.id == food_item_id),
+async def get_single_food_item(food_item_id: str):
+    """Get a specific food item by their ID
+
+    Args:
+
+        food_item_id (str): uuid of the food item.
+
+    Raises:
+        HTTPException: Returns a 404 response if food item does not exist.
+    """
+    for food_item in food_items_data:
+        if food_item.get("id") == food_item_id:
+            return food_item
+    raise HTTPException(
+        status_code=status.HTTP_404_NOT_FOUND, detail="Food item not found."
     )
-    food_item = result.scalars().first()
-    if food_item:
-        return food_item
-    raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Food Item not found.")
+
+
+@router.patch("/{food_item_id}")
+async def update_food_item(food_item_id: str, food_item_update_data: FoodItemUpdate):
+    for food_item in food_items_data:
+        if food_item["id"] == food_item_id:
+            food_item["name"] = food_item_update_data.name
+            food_item["brand"] = food_item_update_data.brand
+            food_item["barcode"] = food_item_update_data.barcode
+            food_item["category"] = food_item_update_data.category
+            food_item["image_url"] = food_item_update_data.image_url
+            return food_item
+    raise HTTPException(
+        status_code=status.HTTP_404_NOT_FOUND, detail="Food item not found."
+    )
+
+
+@router.delete("/{food_item_id}", status_code=status.HTTP_204_NO_CONTENT)
+async def delete_food_item(food_item_id: str):
+    for food_item in food_items_data:
+        if food_item["id"] == food_item_id:
+            food_items_data.remove(food_item)
+            return {}
+    raise HTTPException(
+        status_code=status.HTTP_404_NOT_FOUND, detail="Food item not found."
+    )
---
+++ backend/app/food_items/schema.py
@@ -0,0 +1,31 @@
+import uuid
+from pydantic import BaseModel, ConfigDict, EmailStr, Field
+
+
+# Food Item Schema (for initial crud implementation)
+class FoodItemBase(BaseModel):
+    """Food Item Base properties to be shared"""
+
+    name: str = Field(..., max_length=50)
+    brand: str | None = Field(None, max_length=30)
+    barcode: str | None = Field(None, max_length=100)
+    category: str | None = Field(None, max_length=30)
+    image_url: str | None = Field(None)
+
+
+class FoodItemCreate(FoodItemBase):
+    pass
+
+
+class FoodItemUpdate(BaseModel):
+    name: str | None = Field(None, max_length=50)
+    brand: str | None = Field(None, max_length=30)
+    barcode: str | None = Field(None, max_length=100)
+    category: str | None = Field(None, max_length=30)
+    image_url: str | None = Field(None)
+
+
+class FoodItemResponse(FoodItemBase):
+    model_config = ConfigDict(from_attributes=True)
+
+    id: uuid.UUID
