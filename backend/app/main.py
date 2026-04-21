ebae9022dec06f03f89b6e3aa51ecb7b9ccf8210
klu <52183673+klu04@users.noreply.github.com>
2026-04-20 19:08:11 -0700

    Migrate food items API to async SQLAlchemy > - Replace mock data food_items with async SQLAlchemy to connect to local psql db > - Add lifespan context manager to handle DB engine start and shutdown > - Get food items endpoints using APIRouter

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,63 +1,39 @@
-from uuid import uuid4
-from app.food_data import food_items
-from fastapi import FastAPI, status
-from fastapi.exceptions import HTTPException
-from pydantic import BaseModel
+# Name: Krystal Lu (klu04)
+# Citation for async integration
+# Date: 04/19/2025
+# Adapted from "Python FastAPI Tutorial (Part 7): Sync vs Async - Converting Your App to Asynchronous"
+# Source URL: https://www.youtube.com/watch?v=2JPDt-Jp6fM&list=PL-osiE80TeTsak-c-QsVeg0YYG_0TeyXI&index=8
 
-app = FastAPI()
+from contextlib import asynccontextmanager
+from typing import Annotated
 
-# Food Item Schema (for initial crud implementation)
-class FoodItemBase(BaseModel):
-    name: str
-    brand: str
-    barcode: str 
-    category: str 
+from app.database.sqlconnector import Base, close_db, get_db, init_db
+from app.food_items import router
+from fastapi import FastAPI, Depends, Request
+from sqlalchemy.ext.asyncio import AsyncSession
+import app.database.models
 
-class FoodItemCreate(FoodItemBase):
-    pass
 
-class FoodItemResponse(BaseModel):
-    id: str
-    image_url: str
-    
-    
-# Routers
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    # Start up database engine
+    db_engine = await init_db()
+
+    # Create database tables
+    async with db_engine.begin() as conn:
+        await conn.run_sync(Base.metadata.create_all)
+
+    yield
+
+    # Shutdown database engine
+    await close_db()
+
+
+app = FastAPI(lifespan=lifespan)
+
+app.include_router(router.router, prefix="/v1/food-items", tags=["Food Items"])
+
+
 @app.get("/")
-async def read_msg():
-    return {"msg": "Navigate to food-items"}
-
-@app.get("/food-items", response_model=list[FoodItemBase])
-async def get_all_food_items():
-    """Read all food items."""
-    return food_items
-
-@app.get("/food-items/{food_item_id}", response_model=FoodItemBase)
-def get_single_food_item(food_item_id: str):
-    """Get a specific food item by their ID
-
-    Args:
-        food_item_id (str): uuid of the food item.
-
-    Raises:
-        HTTPException: Returns a 404 response if food item does not exist.
-    """
-    for food_item in food_items:
-        if food_item.get("id") == food_item_id:
-            return food_item
-    raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Food Item not found")
-
-@app.post("/food-items", response_model=FoodItemResponse, status_code=status.HTTP_201_CREATED)
-async def create_food_item(food_item: FoodItemCreate):
-    # Creates a uuid
-    new_id = uuid4()
-    new_food_item = {
-        "id": str(new_id), # Convert UUID to a string
-        "name": food_item.name,
-        "brand": food_item.brand,
-        "barcode": food_item.barcode,
-        "category": food_item.category,
-        "image_url": "https://image-of-food.com/products/",
-    }
-    
-    food_items.append(new_food_item)
-    return new_food_item
+async def home(db: Annotated[AsyncSession, Depends(get_db)]):
+    return {"message": "Connected to database."}
