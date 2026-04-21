f9c5aab0fc2e3d5b9175f0b1148a0177c1352aa5
klu <52183673+klu04@users.noreply.github.com>
2026-04-21 15:52:49 -0700

    Implement update operation for food items

--- backend/app/food_items/router.py
+++ backend/app/food_items/router.py
@@ -3,12 +3,12 @@ from uuid import uuid4
 from fastapi import APIRouter, HTTPException, status
 
 from ..food_items.food_data import food_items_data
-from ..food_items.schema import FoodItemCreate, FoodItemResponse
+from ..food_items.schema import FoodItemCreate, FoodItemResponse, FoodItemUpdate
 
 router = APIRouter()
 
 
-@router.get("", response_model=FoodItemResponse)
+@router.get("", response_model=list[FoodItemResponse])
 async def get_all_food_items():
     """Read all food items."""
     return food_items_data
@@ -22,7 +22,7 @@ async def get_single_food_item(food_item_id: str):
         food_item_id (str): uuid of the food item.
 
     Raises:
-        HTTPException: Returns a 404 response if food item does not exist.
+        HTTPException: 404 response if food item does not exist.
     """
     for food_item in food_items_data:
         if food_item.get("id") == food_item_id:
@@ -47,6 +47,27 @@ async def create_food_item(food_item: FoodItemCreate):
 
     food_items_data.append(new_food_item)
     return new_food_item
+
+
+@router.patch("/{food_item_id}")
+async def partial_update_food_item(food_item_id: str, food_item_data: FoodItemUpdate):
+    """Partially updates a food item by its ID
+
+    - Only include fields that you want to update in the request body.
+
+    - Omit any fields that remain unchanged.
+
+    Raises:
+        HTTPException: 404 response if food item does not exist.
+    """
+    for food_item in food_items_data:
+        if food_item["id"] == food_item_id:
+            # Only update fields that were sent in request
+            food_item.update(food_item_data.model_dump(exclude_unset=True))
+            return food_item
+    raise HTTPException(
+        status_code=status.HTTP_404_NOT_FOUND, detail="Food item not found."
+    )
 
 
 @router.delete("/{food_item_id}", status_code=status.HTTP_204_NO_CONTENT)
