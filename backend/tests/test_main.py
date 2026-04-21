2f9797d128e168b43bbecd8c200e8bf8e9d8459b
klu <52183673+klu04@users.noreply.github.com>
2026-04-20 21:41:18 -0700

    Refactor tests for async functions

--- backend/tests/test_main.py
+++ backend/tests/test_main.py
@@ -1,11 +1,23 @@
-from fastapi.testclient import TestClient
+# Name: Krystal Lu (klu04)
+# Citation for async testing
+# Date: 04/20/2025
+# Adapted from "FastAPI - Async Tests" and "asgi-lifespan"
+# Source URL (1): https://fastapi.tiangolo.com/advanced/async-tests/
+# Source URL (2): https://github.com/florimondmanca/asgi-lifespan#usage
+
+import pytest
+from asgi_lifespan import LifespanManager
+from httpx import ASGITransport, AsyncClient
 
 from app.main import app
 
-client = TestClient(app)
 
-
-def test_read_msg():
-    response = client.get("/")
+@pytest.mark.anyio
+async def test_read_msg():
+    async with LifespanManager(app):
+        async with AsyncClient(
+            transport=ASGITransport(app=app), base_url="http://test"
+        ) as client:
+            response = await client.get("/")
     assert response.status_code == 200
-    assert response.json() == {"msg": "Connected to database."}
+    assert response.json() == {"message": "Connected to database."}
