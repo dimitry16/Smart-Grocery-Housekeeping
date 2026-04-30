ba7b11dbe343defaf4f6d0a0524b4424ac28c405
klu <52183673+klu04@users.noreply.github.com>
2026-04-29 18:11:30 -0700

    Create config file to connect to database

---
+++ backend/app/config.py
@@ -0,0 +1,28 @@
+from typing import Literal
+
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+
+class Settings(BaseSettings):
+    model_config = SettingsConfigDict(
+        env_file=".env",
+        env_file_encoding="utf-8",
+    )
+    ENVIRONMENT: Literal["local", "production"] = "local"
+
+    API_V1_URL: str = "/v1"
+
+    # Local PostgreSQL DB
+    DATABASE_URL: str
+
+    # Production (Cloud SQL)
+    INSTANCE_CONNECTION_NAME: str = ""
+    DB_USER: str = ""
+    DB_PASS: str = ""
+    DB_NAME: str = ""
+
+    # Frontend
+    FRONTEND_URL: str = "http://localhost:5173"
+
+
+settings = Settings()
