268d2055f5c29e2094c582b48e1c663a86065ce0
klu <52183673+klu04@users.noreply.github.com>
2026-04-29 18:43:19 -0700

    Add lifespan function to handle database connection pooling

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,8 +1,23 @@
+from contextlib import asynccontextmanager
+
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
+
+import app.database.models
+from app.database.sqlconnector import close_db
 from app.food_items import router
 
-app = FastAPI()
+
+# FastAPI Docs - Lifespan: https://fastapi.tiangolo.com/advanced/events/#lifespan-events
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+
+    yield
+    # Shutdown database engine
+    await close_db()
+
+
+app = FastAPI(lifespan=lifespan)
 
 app.include_router(router.router, prefix="/v1/food-items", tags=["Food Items"])
 
