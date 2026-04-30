7eda7bd64d14b314350ac8e161da634ba11f2771
klu <52183673+klu04@users.noreply.github.com>
2026-04-29 20:05:16 -0700

    refactor: connect get and create food items operations to database with SQLAlchemy ORM

--- backend/app/food_items/router.py
+++ backend/app/food_items/router.py
@@ -1,6 +1,12 @@
-from uuid import uuid4
+from typing import Annotated
+from uuid import UUID
 
-from fastapi import APIRouter, HTTPException, status
+from fastapi import APIRouter, Depends, HTTPException, status
+from sqlalchemy import select
+from sqlalchemy.ext.asyncio import AsyncSession
+
+from app.database.models import FoodItems as FoodItemsModel
+from app.database.sqlconnector import get_db
 
 from ..food_items.food_data import food_items_data
 from ..food_items.schema import (
@@ -13,42 +19,45 @@ router = APIRouter()
 
 
 @router.get("", response_model=list[FoodItemResponse])
-async def get_all_food_items() -> list:
+async def get_all_food_items(db: Annotated[AsyncSession, Depends(get_db)]):
     """Read all food items."""
-    return food_items_data
+    result = await db.execute(
+        select(FoodItemsModel).order_by(FoodItemsModel.name.asc())
+    )
+    food_items = result.scalars().all()
+    return food_items
 
 
 @router.get("/{food_item_id}", response_model=FoodItemResponse)
-async def get_single_food_item(food_item_id: str) -> dict:
+async def get_single_food_item(
+    food_item_id: UUID, db: Annotated[AsyncSession, Depends(get_db)]
+):
     """Get a specific food item by their ID.
 
     Raises:
         HTTPException: 404 response if food item does not exist.
     """
-    for food_item in food_items_data:
-        if food_item.get("id") == food_item_id:
-            return food_item
+    result = await db.execute(
+        select(FoodItemsModel).where(FoodItemsModel.id == food_item_id),
+    )
+    food_item = result.scalars().first()
+    if food_item:
+        return food_item
     raise HTTPException(
-        status_code=status.HTTP_404_NOT_FOUND, detail="Food Item not found"
+        status_code=status.HTTP_404_NOT_FOUND, detail="Food Item not found."
     )
 
 
 @router.post("", response_model=FoodItemResponse, status_code=status.HTTP_201_CREATED)
-async def create_food_item(food_item: FoodItemCreate) -> dict:
+async def create_food_item(
+    food_item: FoodItemCreate, db: Annotated[AsyncSession, Depends(get_db)]
+):
     """Create a food item."""
 
-    # Creates a uuid
-    new_id = uuid4()
-    new_food_item = {
-        "id": str(new_id),  # Convert UUID to a string
-        "name": food_item.name,
-        "brand": food_item.brand,
-        "barcode": food_item.barcode,
-        "category": food_item.category,
-        "image_url": "https://image-of-food.com/products/",
-    }
-
-    food_items_data.append(new_food_item)
+    new_food_item = FoodItemsModel(**food_item.model_dump())
+    db.add(new_food_item)
+    await db.commit()
+    await db.refresh(new_food_item)
     return new_food_item
 
 
