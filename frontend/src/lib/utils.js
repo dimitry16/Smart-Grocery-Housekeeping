47c1880e9e13267cf98c07764361ae5bdaa79df6
Zilin Xu <xuzili@oregonstate.edu>
2026-05-06 02:31:49 -0700

    Fix frontend: remove submodule and add as normal folder

---
+++ frontend/src/lib/utils.js
@@ -0,0 +1,9 @@
+// Auto-generated with shadcn setup
+// Merges Tailwind classes and resolves css conflicts
+
+import { clsx } from "clsx";
+import { twMerge } from "tailwind-merge"
+
+export function cn(...inputs) {
+  return twMerge(clsx(inputs));
+}
