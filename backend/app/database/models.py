9c3d9a13913760f4c2a87becd092dd02799e50b3
klu <52183673+klu04@users.noreply.github.com>
2026-05-08 21:10:29 -0700

    Refactor Database to remove UserFoodItem - User and FoodItem now have a 1-N relationship - Update primary key and foreign keys and added PSQL uuid generation - Update email to have max_length of 255

--- backend/app/database/models.py
+++ backend/app/database/models.py
@@ -1,16 +1,20 @@
 # Name: Yasser Hernandez (hernayas)
-# Citation for the ode below:
-# Date: 04/15/2025
+# Citation for the code below:
+# Date: 04/15/2026
 # Adapted from "ORM Quick Start"
 # Source URL: https://docs.sqlalchemy.org/en/20/orm/quickstart.html
 
+# Name: Krystal Lu (klu04)
+# Title: Removed UserFoodItem table and updated column constraints
+# Date: 05/08/2026
+
 import datetime
-import uuid
 from decimal import Decimal
 from typing import List, Optional
+from uuid import UUID
 
 import sqlalchemy
-from sqlalchemy import ForeignKey, Numeric, String, Text, UniqueConstraint
+from sqlalchemy import ForeignKey, String, Text, UniqueConstraint, text, types
 from sqlalchemy.orm import Mapped, mapped_column, relationship
 
 from app.database.sqlconnector import Base
@@ -18,54 +22,48 @@ from app.database.sqlconnector import Base
 
 class User(Base):
     __tablename__ = "users"
-    id: Mapped[uuid.UUID] = mapped_column(primary_key=True, default=uuid.uuid4)
+    id: Mapped[UUID] = mapped_column(
+        types.Uuid, primary_key=True, server_default=text("gen_random_uuid()")
+    )
     email_address: Mapped[str] = mapped_column(String(255), nullable=False, unique=True)
     password_hash: Mapped[str] = mapped_column(String(255), nullable=False)
     name: Mapped[Optional[str]] = mapped_column(String(30))
 
-    user_food_items: Mapped[List["UserFoodItem"]] = relationship(
-        back_populates="user", cascade="all, delete-orphan"
+    food_items: Mapped[List["FoodItem"]] = relationship(
+        back_populates="owner", cascade="all, delete-orphan"
     )
-
-
-class UserFoodItem(Base):
-    __tablename__ = "user_food_items"
-    id: Mapped[uuid.UUID] = mapped_column(primary_key=True, default=uuid.uuid4)
-    user_id: Mapped[uuid.UUID] = mapped_column(
-        ForeignKey("users.id", ondelete="CASCADE"), nullable=False
-    )
-    food_item_id: Mapped[uuid.UUID] = mapped_column(
-        ForeignKey("food_items.id"), nullable=False
-    )
-
-    quantity: Mapped[Decimal] = mapped_column(
-        sqlalchemy.Numeric(6, 2), nullable=False, default=1
-    )
-    unit: Mapped[Optional[str]] = mapped_column(String(30))
-    expiration_date: Mapped[Optional[datetime.date]] = mapped_column(sqlalchemy.Date)
-
-    # Ensure uniqueness so duplicate items are not added
-    __table_args__ = (
-        UniqueConstraint("user_id", "food_item_id", name="user_food_id_uc"),
-    )
-
-    user: Mapped["User"] = relationship(back_populates="user_food_items")
-    food_item: Mapped["FoodItem"] = relationship()
 
 
 class FoodItem(Base):
     __tablename__ = "food_items"
-    id: Mapped[uuid.UUID] = mapped_column(primary_key=True, default=uuid.uuid4)
+    id: Mapped[UUID] = mapped_column(
+        types.Uuid, primary_key=True, server_default=text("gen_random_uuid()")
+    )
+    user_id: Mapped[UUID] = mapped_column(
+        types.Uuid, ForeignKey("users.id", ondelete="CASCADE"), nullable=False
+    )
     name: Mapped[str] = mapped_column(String(50), nullable=False)
     brand: Mapped[Optional[str]] = mapped_column(String(30))
-    barcode: Mapped[Optional[str]] = mapped_column(String(100), unique=True)
+    barcode: Mapped[Optional[str]] = mapped_column(String(100))
     category: Mapped[Optional[str]] = mapped_column(String(30))
     image_url: Mapped[Optional[str]] = mapped_column(Text)
+    quantity: Mapped[Decimal] = mapped_column(
+        sqlalchemy.Numeric(6, 2), nullable=False, default=Decimal("1.00")
+    )
+    unit: Mapped[Optional[str]] = mapped_column(String(30))
+    expiration_date: Mapped[Optional[datetime.date]] = mapped_column(sqlalchemy.Date)
+
+    # Ensure uniqueness so no same barcode is added twice.
+    __table_args__ = (UniqueConstraint("user_id", "barcode", name="user_barcode"),)
+
+    owner: Mapped["User"] = relationship(back_populates="food_items")
 
 
 class Recipe(Base):
     __tablename__ = "recipes"
-    id: Mapped[uuid.UUID] = mapped_column(primary_key=True, default=uuid.uuid4)
+    id: Mapped[UUID] = mapped_column(
+        types.Uuid, primary_key=True, server_default=text("gen_random_uuid()")
+    )
     title: Mapped[str] = mapped_column(String(50), nullable=False)
     description: Mapped[Optional[str]] = mapped_column(String(600))
     image_url: Mapped[Optional[str]] = mapped_column(Text)
@@ -78,16 +76,18 @@ class Recipe(Base):
 
 class RecipeIngredient(Base):
     __tablename__ = "recipe_ingredients"
-    id: Mapped[uuid.UUID] = mapped_column(primary_key=True, default=uuid.uuid4)
-    recipe_id: Mapped[uuid.UUID] = mapped_column(
-        ForeignKey("recipes.id", ondelete="CASCADE"), nullable=False
+    id: Mapped[UUID] = mapped_column(
+        types.Uuid, primary_key=True, server_default=text("gen_random_uuid()")
     )
-    food_item_id: Mapped[uuid.UUID] = mapped_column(
-        ForeignKey("food_items.id"), nullable=False
+    recipe_id: Mapped[UUID] = mapped_column(
+        types.Uuid, ForeignKey("recipes.id", ondelete="CASCADE"), nullable=False
+    )
+    food_item_id: Mapped[UUID] = mapped_column(
+        types.Uuid, ForeignKey("food_items.id"), nullable=False
     )
 
-    quantity: Mapped[Numeric] = mapped_column(
-        sqlalchemy.Numeric(10, 2), nullable=False, default=1
+    quantity: Mapped[Decimal] = mapped_column(
+        sqlalchemy.Numeric(10, 2), nullable=False, default=Decimal("1.00")
     )
     unit: Mapped[Optional[str]] = mapped_column(String(30))
 
