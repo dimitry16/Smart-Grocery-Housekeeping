f3306bd73830492d639d81b4645371b1737a636a
klu <52183673+klu04@users.noreply.github.com>
2026-05-14 14:10:44 -0700

    Users schema change to setup auth

--- backend/app/users/schema.py
+++ backend/app/users/schema.py
@@ -12,6 +12,17 @@ class UserCreate(UserBase):
     password: str = Field(min_length=8)
 
 
+class UserPublic(BaseModel):
+    model_config = ConfigDict(from_attributes=True)
+
+    id: uuid.UUID
+    name: str | None
+
+
+class UserPrivate(UserPublic):
+    email_address: EmailStr
+
+
 class UserUpdate(BaseModel):
     name: str | None = Field(default=None, min_length=2, max_length=30)
     email_address: EmailStr | None = Field(default=None, max_length=255)
