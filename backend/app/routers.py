663d63c1d96b6ba937385ef3de3e2e659fba0b8a
Yasser Hernandez <dimitry202@gmail.com>
2026-05-14 17:30:00 -0800

    added vision router

--- backend/app/routers.py
+++ backend/app/routers.py
@@ -2,6 +2,7 @@ from fastapi import APIRouter
 
 from app.food_items.router import router as food_items_router
 from app.users.router import router as users_router
+from app.external_api_services.vision_router import router as vision_router
 
 api_router = APIRouter()
 
@@ -11,3 +12,4 @@ api_router.include_router(
     tags=["Food Items"],
 )
 api_router.include_router(users_router, prefix="/users", tags=["users"])
+api_router.include_router(vision_router, prefix="/vision", tags=["Vision"])
