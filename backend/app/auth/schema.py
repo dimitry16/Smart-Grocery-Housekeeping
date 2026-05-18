e63fca5558190387ffd6555c1a7116e72d44100b
klu <52183673+klu04@users.noreply.github.com>
2026-05-18 15:29:28 -0700

    Add TokenData to validate JWT token payload

--- backend/app/auth/schema.py
+++ backend/app/auth/schema.py
@@ -1,6 +1,13 @@
+from uuid import UUID
+
 from pydantic import BaseModel
 
 
 class Token(BaseModel):
     access_token: str
     token_type: str
+
+
+class TokenData(BaseModel):
+    sub: UUID
+    exp: int
