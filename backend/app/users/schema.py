d5016533794b9759fad73ddcd300c6a0fa220ff4
zilinxuOSU <xuzili@oregonstate.edu>
2026-05-23 06:11:31 -0700

    Add files via upload

---
+++ backend/app/users/schema.py
@@ -0,0 +1,25 @@
+import uuid
+
+from pydantic import BaseModel, ConfigDict, EmailStr, Field
+
+
+class UserBase(BaseModel):
+    name: str | None = Field(min_length=2, max_length=30)
+    email_address: EmailStr = Field(max_length=255)
+
+
+class UserCreate(UserBase):
+    password: str = Field(min_length=8)
+
+
+class UserUpdate(BaseModel):
+    name: str | None = Field(default=None, min_length=2, max_length=30)
+    email_address: EmailStr | None = Field(default=None, max_length=255)
+
+
+class UserResponse(UserBase):
+    model_config = ConfigDict(from_attributes=True)
+
+    id: uuid.UUID
+    name: str | None
+    email_address: EmailStr
