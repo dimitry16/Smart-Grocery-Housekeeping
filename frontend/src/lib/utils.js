c587f69d4e1a90daff36dd45575221dc0de81908
zilinxuOSU <xuzili@oregonstate.edu>
2026-05-23 06:11:50 -0700

    Delete frontend directory

--- frontend/src/lib/utils.js
+++
@@ -1,9 +0,0 @@
-// Auto-generated with shadcn setup
-// Merges Tailwind classes and resolves css conflicts
-
-import { clsx } from "clsx";
-import { twMerge } from "tailwind-merge"
-
-export function cn(...inputs) {
-  return twMerge(clsx(inputs));
-}
