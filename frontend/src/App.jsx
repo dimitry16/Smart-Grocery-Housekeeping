f93735e1918a81ed9dcbec5df0f8d36bfc800b26
p-tica <165862113+p-tica@users.noreply.github.com>
2026-05-24 14:15:51 -0700

    Update App to match PR 51

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -11,6 +11,7 @@ import Navbar from '@/components/ui/navbar'
 import AddItem from './pages/AddItem';
 import CurrentItems from './pages/CurrentItems';
 import Recipes from './pages/Recipes';
+import SavedRecipes from './pages/SavedRecipes';
 import Reports from './pages/Reports';
 import Login from './pages/Login';
 import BarcodeScanner from './pages/BarcodeScanner';
@@ -21,7 +22,7 @@ function App() {
     <BrowserRouter>
       <div className="flex">
         <Navbar />
-        <main className="flex-1">
+        <main className="flex-1 md:ml-48">
           <Routes>
             <Route path="/" element={<Dashboard />} />
             <Route path="/additem" element={<AddItem />} />
@@ -29,6 +30,7 @@ function App() {
             <Route path="/scan-object" element={<ObjectScanner />} />
             <Route path="/current_items" element={<CurrentItems />} />
             <Route path="/recipes" element={<Recipes/>} />
+            <Route path="/savedrecipes" element={<SavedRecipes/>} />
             <Route path="/reports" element={<Reports/>} />
             <Route path="/login" element={<Login/>} />
           </Routes>
