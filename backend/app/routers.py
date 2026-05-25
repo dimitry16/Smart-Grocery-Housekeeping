c7fe43f53a2495634dc9b98b53bf513cd1ee0613
zilinxuOSU <xuzili@oregonstate.edu>
2026-05-24 20:14:21 -0700

    Update routers.py

--- backend/app/routers.py
+++ backend/app/routers.py
@@ -2,6 +2,7 @@ from fastapi import APIRouter
 
 from app.auth.router import router as auth_router
 from app.food_items.router import router as food_items_router
+from app.reports.router import router as reports_router
 from app.users.router import router as users_router
 from app.external_api_services.router import router as vision_router
 from app.recipes.router import router as recipe_router
@@ -16,5 +17,6 @@ api_router.include_router(
     tags=["Food Items"],
 )
 api_router.include_router(users_router, prefix="/users", tags=["users"])
+api_router.include_router(reports_router, tags=["Reports"])
 api_router.include_router(vision_router, prefix="/vision", tags=["Vision"])
 api_router.include_router(recipe_router, prefix="/recipes", tags=["Recipes"])
