870e9313ec3a3bdae314457277de82423708dacf
Yasser Hernandez <dimitry202@gmail.com>
2026-05-29 17:51:55 -0800

    removed unused import

--- backend/alembic/versions/ae54dbf86651_added_user_id_to_recipes.py
+++ backend/alembic/versions/ae54dbf86651_added_user_id_to_recipes.py
@@ -8,9 +8,6 @@ Create Date: 2026-05-28 17:10:59.786400
 
 from typing import Sequence, Union
 
-from alembic import op
-import sqlalchemy as sa
-
 
 # revision identifiers, used by Alembic.
 revision: str = "ae54dbf86651"
