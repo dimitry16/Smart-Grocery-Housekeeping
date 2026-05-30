9fd8b7980cc1c5e17c41865e1fbfe6b2d7439552
Yasser Hernandez <dimitry202@gmail.com>
2026-05-29 19:56:21 -0800

    ruff format

--- backend/alembic/versions/58e7afc45092_merge_branching_migrations.py
+++ backend/alembic/versions/58e7afc45092_merge_branching_migrations.py
@@ -9,7 +9,6 @@ Create Date: 2026-05-29 19:36:37.838926
 from typing import Sequence, Union
 
 
-
 # revision identifiers, used by Alembic.
 revision: str = "58e7afc45092"
 down_revision: Union[str, Sequence[str], None] = ("51ab6a0e2152", "b7e2a1f3d890")
